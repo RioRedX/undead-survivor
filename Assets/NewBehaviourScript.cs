fb7d5944d170f6ac5c2ebb7f034ffac633315863
HJ K <rngudwns0215@naver.com>
2023-04-07 18:29:49 +0900

    First Git

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int a;
+    //gkgk 하항;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
