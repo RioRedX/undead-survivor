bc57600f4577b3cb9eec858c2866b6899c661011
HJ K <rngudwns0215@naver.com>
2023-04-08 15:12:39 +0900

    3강 애니메이션 + 오버라이드

--- Assets/Undead Survivor/Code/Player.cs
+++ Assets/Undead Survivor/Code/Player.cs
@@ -9,15 +9,18 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-
         Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
     }
@@ -26,5 +29,15 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0 ? true : false;
+        }
     }
 }
