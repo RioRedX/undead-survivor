f145f1740e93f81aa0f81a1a0bee888ada92c6e7
HJ K <rngudwns0215@naver.com>
2023-04-10 21:40:21 +0900

    9강 타격감있는 몬스터 처치

--- Assets/Undead Survivor/Code/Enemy.cs
+++ Assets/Undead Survivor/Code/Enemy.cs
@@ -13,32 +13,39 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriter;
+    WaitForFixedUpdate wait;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
     
     // 물리관련 == rigid 사용
     void FixedUpdate()
     {
-        if (!isLive)
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
             return;
 
         Vector2 dirVec = target.position - rigid.position;
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);   // 기본 좌표이동
         // 충돌시 밀려남(속도가 있다는 뜻) 처리
-        rigid.velocity = Vector2.zero;
+        rigid.velocity = Vector2.zero; 
     }
 
     // 후처리
     void LateUpdate()
     {
+        if (!isLive)
+            return;
+
         spriter.flipX = target.position.x < rigid.position.x;
     }
 
@@ -48,6 +55,10 @@ public class Enemy : MonoBehaviour
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         // 생존여부와 체력 초기화
         isLive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
         //health = maxHealth;
     }
 
@@ -62,7 +73,7 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet"))
+        if (!collision.CompareTag("Bullet") || !isLive) // isLive : 죽은 후 빠르게 충돌시 else부분이 2번 이상 호출 가능 
             return;
 
         health -= collision.GetComponent<Bullet>().damage;
@@ -70,12 +81,35 @@ public class Enemy : MonoBehaviour
         if (health > 0)
         {
             // .. Live, Hit Action
+            // 애니메이션, 넉백 추가
+            anim.SetTrigger("Hit");
+
+            StartCoroutine(KnockBack()); // "KnockBack"도 가능
         }
         else
         {
             // .. Die
-            Dead();
+            //! => 재활용하기 위해선 값을 원래로 돌려놔야한다.
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;   // 다른 적 아래로
+            anim.SetBool("Dead", true);
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
         }
+    }
+
+    IEnumerator KnockBack()
+    {
+        // yield return null; <- 1 프레임 쉬기
+        // yield return new WaitForSeconds(2f); <- 2초 쉬기. *new를 자주 쓰면 최적화에 Bad=> 함수화해서 사용할 것
+        yield return wait; // 하나의 물리 프레임을 딜레이(1 프레임 쉬기)
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+
+
     }
 
     void Dead()
--- Assets/Undead Survivor/Code/GameManager.cs
+++ Assets/Undead Survivor/Code/GameManager.cs
@@ -6,12 +6,20 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [Header("# Game Control")]
     public float gameTime;
     public float maxGameTime = 2 * 10f;
 
+    //처치 데이터
+    [Header("# Player Info")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 3, 5, 10, 30, 60, 100, 150, 210, 280, 360, 450, 600};
+
+    [Header("# Game Obejct")]
     public PoolManager pool;    //todo:pool->poolManager
     public Player player;
-
 
     void Awake()
     {
@@ -25,6 +33,16 @@ public class GameManager : MonoBehaviour
         if (gameTime > maxGameTime)
         {
             gameTime = maxGameTime;
+        }
+    }
+
+    public void GetExp()
+    {
+        exp++;
+
+        if (exp == nextExp[level]) {
+            level++;
+            exp = 0;
         }
     }
 
